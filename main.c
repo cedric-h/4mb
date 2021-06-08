fb913335fac29c2bad114e9faca5f0d526109dca
Vinícius Viana <vinicius.viana1992@gmail.com>
2021-06-08 18:03:38 -0300

    Fixed tree_generation function location

--- main.c
+++ main.c
@@ -36,7 +36,6 @@ extern __declspec(dllimport) float sqrtf(float x);
 
 #include "err.h"
 #include "box.h"
-#include "tree.h"
 
 /* based on scancodes */
 typedef enum {
@@ -206,17 +205,39 @@ static void player_try_jump() {
         state.player.jump_cooldown = 0;
 }
 
+static void treegen_recursive(BoxId box, int n, Vec3 center) {
+    if (n < 0) return;
+
+    Vec3 box_pos = box_pos_to_vec3(boxes[box].pos);    
+    if (sdf_sphere3(sub3(box_pos, center), 2.0f) > 0.0f) return;
+
+    BoxId left = add_box(box, Face_Left, BoxKind_Dirt);
+    treegen_recursive(left, n - 1, center);
+
+    BoxId right = add_box(box, Face_Right, BoxKind_Dirt);
+    treegen_recursive(right, n - 1, center);
+
+    BoxId front = add_box(box, Face_Front, BoxKind_Dirt);
+    treegen_recursive(front, n - 1, center);
+
+    BoxId back = add_box(box, Face_Back, BoxKind_Dirt);
+    treegen_recursive(back, n - 1, center);
+
+    BoxId above = add_box(box, Face_Above, BoxKind_Dirt);
+    treegen_recursive(above, n - 1, center);
+}
+
 static void init_world() {
     /* moved somewhere and pulled these values out of debugger */
-    state.player.pos = vec3(-9.5f, 2.0f, -9.5f);
+    state.player.pos = vec3(-8.5f, 2.0f, 0.5f);
     state.cam.yaw = 0.818;
     state.cam.pitch = -0.88;
 
     #define ORIGIN 1
     boxes[ORIGIN] = (Box) {
         .kind = BoxKind_Dirt,
-        .pos.x = -10.0f,
-        .pos.z = -10.0f,
+        .pos.x = -9.0f,
+        .pos.z = -0.0f,
     };
     add_box(ORIGIN, Face_Left, BoxKind_Dirt);
     add_box(ORIGIN, Face_Right, BoxKind_Dirt);
@@ -228,7 +249,8 @@ static void init_world() {
     };    
 
     Vec3 box_pos = box_pos_to_vec3(boxes[last_box + 1].pos);
-    treegen_recursive(last_box + 1, 10, box_pos);
+    box_pos.y += 2;
+    treegen_recursive(last_box + 1, 4, box_pos);
 }
 
 
